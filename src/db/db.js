4f52312e1168e04cc3daeb552325cdd0ad2fe12a
Premkmaurya <premkmaurya123456@gmail.com>
2026-01-08 22:49:48 +0530

    feat: update uuid dependency to version 9.0.1 and improve database connection handling

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 function connectDB() {
   try {
+    if (isConnected) return;
     mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
     console.log("db is connected.");
   } catch (error) {
     console.log("db is not connected.");
